8f21d081f8397c342adf050b47e546e94941125f
Luciano Peñafiel <luciano.penafiel@aerolab.co>
2022-01-15 12:14:55 -0300

    install react navigation

--- android/app/src/main/java/com/unocalculatorrn/MainActivity.java
+++ android/app/src/main/java/com/unocalculatorrn/MainActivity.java
@@ -1,6 +1,7 @@
 package com.unocalculatorrn;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +12,13 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "unoCalculatorRN";
+  }
+
+  /**
+   * React Navigition
+   */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
