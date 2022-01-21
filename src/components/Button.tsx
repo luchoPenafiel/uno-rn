0c461bd429674ee7b5892eb5a7271eeb01204308
Luciano Peñafiel <luciano.penafiel@aerolab.co>
2022-01-21 19:52:26 -0300

    feat: add minor styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,13 +8,15 @@ import theme from '@uno/constants/theme';
 
 const Wrapper = styled(Pressable)<{ disabled: boolean }>`
   padding: ${theme.spaces.l}px;
-  margin-top: 10px;
+  margin-top: ${theme.spaces['2xl'] * 2}px;
 
-  background-color: ${({ disabled }) => (disabled ? theme.color.gray : theme.color.green)};
+  background-color: ${({ disabled }) => (disabled ? theme.color.gray : theme.color.blue)};
   border-radius: ${theme.bordeRadius};
 `;
 
-const TextStyled = styled(Text)`
+const TextStyled = styled(Text)<{ disabled: boolean }>`
+  color: ${theme.color.darkGray};
+
   text-align: center;
   font-size: 18px;
 `;
@@ -30,7 +32,7 @@ export const Button = ({
 }) => {
   return (
     <Wrapper disabled={disabled} onPress={onPress}>
-      <TextStyled>{children}</TextStyled>
+      <TextStyled disabled={disabled}>{children}</TextStyled>
     </Wrapper>
   );
 };
