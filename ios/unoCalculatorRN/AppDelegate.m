435756bc84aa508665e5dc4eefbe4307a335141f
Luciano Peñafiel <lucianopenafiel@gmail.com>
2022-01-26 09:18:52 -0300

    feat: add firebase event

--- ios/unoCalculatorRN/AppDelegate.m
+++ ios/unoCalculatorRN/AppDelegate.m
@@ -4,6 +4,8 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <Firebase.h>
+
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
@@ -31,6 +33,7 @@ static void InitializeFlipper(UIApplication *application) {
   InitializeFlipper(application);
 #endif
 
+  [FIRApp configure];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"unoCalculatorRN"
